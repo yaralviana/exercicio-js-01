bf70cd7f2e988b800b83e10c7d7732f8e7b9800f
Yara Viana <yaralviana@gmail.com>
2021-08-25 19:41:42 -0300

    Segue exercícios da primeira aula de JS do curso {reprograma}.

---
+++ exercicio.js
@@ -0,0 +1,8 @@
+var soma = 87 + 96;
+var multiplicacao = 8 * 5;
+var lista = ['Maça', ' Banana', ' Brócolis',' Arroz', ' Feijão', ' TRENTO ♥'];
+
+document.querySelector('#welcome').onclick = function(){alert('Olá, meninas!')};
+document.querySelector('#soma').onclick = function(){alert('O resultado é '+soma)};
+document.querySelector('#multiplicacao').onclick = function(){alert('O resultado é '+multiplicacao)};
+document.querySelector('#lista').onclick = function(){alert('Minhas compras: '+lista)};
